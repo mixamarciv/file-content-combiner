78e05894a026eac0efdd1ba7d6b735177686c495
mixamarciv <mixamarciv@gmail.com>
2025-12-15 21:08:51 +0300

    20251215 +combine directories content

--- .bin/run_cmd_with_env.bat
+++ .bin/run_cmd_with_env.bat
@@ -1,5 +1,13 @@
 @echo off
 set thisPath=%~dp0
+set thisFileName=%~nx0
+set appTitleName=%thisFileName%
+
+if "%1"=="" (
+	:: run this file with title "%appTitleName%" for set position and size from registry
+	cmd /c start "%appTitleName%" /D "%thisPath%" /I %thisFileName% "not_exit"
+	exit
+)
 
 call "%thisPath%\set_env.bat"
 
